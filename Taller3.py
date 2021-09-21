a34f90c4c0c02303ee1a3d69cb03436f0ba28e16
Dewys Eguis <deguis@mangus.org>
2021-09-20 19:37:25 -0500

    Punto n°3

--- Taller3.py
+++ Taller3.py
@@ -27,3 +27,61 @@ def pto1 (aut):
         "Autos verdes": car_verde,
         "Autos azules": car_azul
     }
+
+# Punto 2
+
+def pto2 (animal):
+    edad_animales = []
+    if animal.lower() == 'elefante':
+        for i in range(20):
+            elefante = int(input('Digite la edad del elefante:'))
+            edad_animales.append(elefante)
+    if animal.lower() == 'jirafa':
+        for i in range(15):
+            jirafa = int(input('Digite edad de la jirafa:'))
+            edad_animales.append(jirafa)
+    if animal.lower() == 'chimpance':
+        for i in range(40):
+            chimpance = int(input('Digite la edad del chimpance:'))
+            edad_animales.append(chimpance)
+    
+    c1 = c2 = c3 = 0
+    for edad in edad_animales:
+        if edad >= 0 and edad <= 1:
+            c1 += 1
+        elif edad > 1 and edad < 3:
+            c2 += 1
+        elif edad >= 3:
+            c3 += 1
+    
+    if animal.lower() == 'elefante':
+        c1 = (c1 * 100) / 20 
+        c2 = (c2 * 100) / 20
+        c3 = (c3 * 100) / 20
+        
+    if animal.lower() == 'jirafa':
+        c1 = (c1 * 100) / 15 
+        c2 = (c2 * 100) / 15
+        c3 = (c3 * 100) / 15
+        
+    if animal.lower() == 'chimpance':
+        c1 = (c1 * 100) / 40
+        c2 = (c2 * 100) / 40
+        c3 = (c3 * 100) / 40
+
+
+    return {
+        "animal": animal,
+        "Entre 0 y 1 año": c1,
+        "Entre 1 y 3 años": c2,
+        "Mayor o igual a 3 años": c3
+    }
+
+# Punto 3
+
+def pto3 (h_obr):
+    for obr in h_obr:
+        if obr <= 40:
+            print(f'Pago por 40 horas o menos: {obr * 20}')
+        elif obr > 40:
+            print(f'Pago por cada una de las primeras horas: {40 * 20 + ((obr - 40) * 25)} ')
